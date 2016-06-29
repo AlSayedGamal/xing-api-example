bfed638a8c12cbcc79d9751c807af13233883ef1
AlSayed Gamal <ag@menacommerce.com>
2016-06-29 11:03:22 +0200

    heroku ready

--- config/initializers/xing.rb
+++ config/initializers/xing.rb
@@ -1,4 +1,4 @@
 XingApi::Client.configure do |config|
-  config.consumer_key = "############################"
-  config.consumer_secret = "##########################"
+  config.consumer_key = ENV['XING_TOKEN']
+  config.consumer_secret = ENV['XING_SECRET']
 end
